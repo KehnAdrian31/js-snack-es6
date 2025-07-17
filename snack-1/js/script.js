935782877c89714fb1be5b51f1d987d60d3e4e8a
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-17 13:30:26 +0200

    [upd] add result

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,16 +1,19 @@
 // creo array di oggetti
 let bici = [
     {nome: 'Bianchi', peso: 7},
-    {nome: 'Specialized', peso: 5.7},
-    {nome: 'Trek', peso: 5.5}
+    {nome: 'Cannondale', peso: 5.7},
+    {nome: 'Colnago', peso: 5.5}
 ];
 
-let bicileggera = bici[0];
+let biciLeggera = bici[0];
 
 //ciclo array
-for( let i = 0; i < bici-length; i++){
+for( let i = 0; i < bici.length; i++){
   //controllo quale bici pesa di meno
-  if(bici[i].peso < bicileggera.peso){
+  if(bici[i].peso < biciLeggera.peso){
     biciLeggera = bici[i];
   }
 }
+
+// stampo risultato
+console.log(`La bici più leggera è la ${biciLeggera.nome} con un peso di ${biciLeggera.peso} kg.`)
