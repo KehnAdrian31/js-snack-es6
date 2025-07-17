52a2c49ca428a0d5f7546c2305c73b59b21e04df
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-17 13:38:01 +0200

    [upd] snack2 add random punti e falli

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -5,3 +5,11 @@ const squadreCalcio = [
   {nome: 'Juventus', puntiFatti: 0, falliSubiti: 0},
   {nome: 'Milan', puntiFatti: 0, falliSubiti: 0},
 ];
+
+console.log(squadreCalcio);
+
+//assegno numeri random a puntiFatti e falliSubiti
+for (let i = 0; i < squadreCalcio.length; i++) {
+    squadreCalcio[i].puntiFatti = Math.floor(Math.random() * 81);       // da 0 a 80
+    squadreCalcio[i].falliSubiti = Math.floor(Math.random() * 81);     // da 0 a 80
+}
